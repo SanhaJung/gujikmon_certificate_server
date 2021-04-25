75824aa94dad4eec9bfcd25a38f129b7d81848be
kssgit <76714467+kssgit@users.noreply.github.com>
2021-04-25 18:54:33 +0900

    [UPDATE] delete ven

--- ven/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\msa_cloud(b)\gujikmon_api\gujikmon_certificate_server\ven\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
